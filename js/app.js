5abf9277cbd7534772fcdc67e81a78364d340a13
Gawen Arab <g@wenarab.com>
2019-07-11 16:03:20 +0200

    processors as a service through ws

--- js/app.js
+++ js/app.js
@@ -2,9 +2,108 @@ const alias = require("./alias.js");
 const {google} = require('googleapis');
 const gdrive = require('./gdrive.js');
 
+const ws = require('nodejs-websocket');
+
+class Server {
+    constructor() {
+        this.server = ws.createServer(this._onconnect.bind(this));
+    }
+
+    listen(port) {
+        return this.server.listen(port);
+    }
+
+    _onconnect(conn) {
+        var self = this;
+        conn.on("text", function(d) { self._ontext(conn, d); });
+        conn.on("close", function() { self._onclose(conn); });
+    }
+
+    _onclose(conn) {
+        if (conn.processor) {
+            console.debug("processing cancelled");
+            conn.processor.terminate();
+            delete conn.processor;
+        }
+    }
+
+    _ontext(conn, d) {
+        this._handle(conn, d)
+            .catch(() => console.error())
+            .finally(() => {
+                console.log("exit");
+                conn.close();
+            })
+        ;
+    }
+
+    async _handle(conn, d) {
+        let send = function(obj) {
+            conn.sendText(JSON.stringify(obj));
+        };
+
+        try {
+            var d = JSON.parse(d);
+        } catch(e) {
+            console.error("incorrect format");
+            conn.close();
+            return;
+        }
+
+        try {
+            let scopes = d.scopes.map((s) => new alias.Scope(s[0], s[1], s[2], s[3]));
+
+            var args = {
+                client_url: d.client_url,
+                inp: d.inp,
+                scopes: scopes,
+                onprogress: send,
+            };
+
+        } catch(e) {
+            console.log("bad payload");
+            conn.close();
+            return;
+        }
+
+        console.log("Start processing");
+        console.log(this._describe(args));
+
+        conn.processor = new alias.Processor(args);
+        conn.startDate = new Date();
+        send({"init": true});
+        await conn.processor.init()
+        const res = await conn.processor.run();
+
+        conn.processor.terminate();
+        delete conn.processor;
+
+        const endDate = new Date();
+        const duration = (endDate - conn.startDate) / 1000.0;
+        console.log("Processing finished in " + duration + "s!");
+
+        send(res);
+    }
+
+    _describe(args) {
+        let r = [];
+
+        r.push("... " + args.inp.length + " input(s):\n");
+        args.inp.forEach((inp) => r.push("... - " + inp.url + "\n"));
+        r.push("... with scopes:\n");
+        args.scopes.forEach((scope) => r.push("... - " + scope.provider + "." + scope.path + "\n"));
+        r.push("... to " + args.client_url + "\n");
+
+        return r.join("");
+    }
+}
+
+(new Server()).listen(8000);
+
+/*
 async function main() {
     let inp = null;
-    if (true) {
+    if (false) {
         const auth = await require('./gdrive.js').getHandler()
         const drive = google.drive({version: 'v3', auth});
 
