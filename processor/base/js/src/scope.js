18a1e87e85c8828b9c627c2c0f36d94df1c6ce75
Gawen Arab <g@wenarab.com>
2019-08-07 13:18:28 +0200

    implement predicates for processors

--- processor/base/js/src/scope.js
+++ processor/base/js/src/scope.js
@@ -1,17 +1,34 @@
+let predicateByOps = {
+    "=":   function(e, field, val) { return e[field] === val; },
+    "!=":  function(e, field, val) { return e[field] !== val; },
+    ">":   function(e, field, val) { return e[field] > val; },
+    ">=":  function(e, field, val) { return e[field] >= val; },
+    "<":   function(e, field, val) { return e[field] < val; },
+    "<=":  function(e, field, val) { return e[field] <= val; },
+};
+
 class Scope {
     constructor(provider, path, predicates, fields) {
         this.provider = provider;
         this.path = path;
-        this.predicates = predicates;
+        this.predicates = predicates || [];
         this.fields = fields;
     }
 
     match(e) {
-        if (this.predicates == null) {
-            return true;
+        for (const pred in this.predicates) {
+            const op = pred[0];
+            const predFunc = predicateByOps[op];
+            if (pred === undefined) {
+                throw "unknown predicate operator: " + op;
+            }
+
+            if (!predFunc(e, ...pred.slice(1))) {
+                return false;
+            }
         }
 
-        throw "not implemented";
+        return true;
     }
 
     filterFields(e) {
