b7b3993ed566041286cb7ba3b9903b8e42be98d5
Gawen Arab <g@wenarab.com>
2020-01-06 11:24:24 +0100

    provider/history: rename old term 'orders' to 'grants'

--- provider/src/history.js
+++ provider/src/history.js
@@ -1,9 +1,9 @@
 const redis = require('./redis.js')
 
 async function getHistoryRange(publicKey, start, end) {
-    const orderTokens = await redis.db.lrange(redis.key("user", publicKey, "history"), start, end);
-    const orders = orderTokens.map((tok) => chain.fromSafeToken(tok));
-    return orders;
+    const grantTokens = await redis.db.lrange(redis.key("user", publicKey, "history"), start, end);
+    const grants = grantTokens.map((tok) => chain.fromSafeToken(tok));
+    return grants;
 }
 
 module.exports.getHistoryRange = getHistoryRange;
